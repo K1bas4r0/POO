8382c4d2c3387765ea3d0e1c63cb7c1312a2006f
K1bas4r0 <k1bas4r0@protonmail.com>
2023-10-20 19:31:21 -0500

    Updates Homeworks

--- fibo.js
+++ fibo.js
@@ -1,7 +1,7 @@
 function fibo (n){return n===1 || n===0 ? n : fibo(n-1) + fibo(n-2)}
 
-console.log(fibo(4));
+console.log(fibo(6));
 
 function padovan (n){return n <=3 ? 1 : padovan(n-2) + padovan(n-3)}
 
-console.log(padovan(7));
+console.log(padovan(8));
--- palindromo.js
+++ palindromo.js
@@ -1,10 +1,32 @@
-let a = 'ana';
-let tex = 'fui a roma y encontre mi amor'
+let a = 'naan';
+let tex = 'le dije a ana que ellos narran varias sagas'
+let palInTexMax = 'esto le dije a mi loca colima'
+
+inv = palInTexMax.replace(/\s/g,'').split('');
+swap = palInTexMax.replace(/\s/g,'').split('');
+
+inv = inv.reverse();
+
+jswap = swap.join('');
+jinv = inv.join('');
+
+while (!palin) {
+    if (jswap === jinv) {
+        
+    }
+    swap = swap.join('');
+    inv = inv.join('');
+}
+
+console.log(jswap);
+console.log(ijnv)
+
 let listPalInText = []
 
 function pInText(text) {
     let foText = text.split(' ')
-    foText.forEach(word => { palindromo(word) ? listPalInText.push(word) : pass});
+    foText.forEach(word => { palindromo(word) ? listPalInText.push(word) : 0});
+    return listPalInText
 }
 console.log(pInText(tex));
 
